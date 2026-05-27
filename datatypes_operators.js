0e9070a2f41eb536fa038ea7793833d89f80f378
Thabang Matlou <thabangmatlou29@gmail.com>
2026-05-27 15:41:24 +0200

    Updating datatypes and variables

---
+++ datatypes_operators.js
@@ -0,0 +1,17 @@
+let integer = 10
+let float = 2.5
+
+
+addition = integer + float
+subtraction = integer - float
+multiplication = integer * float
+division = integer / float
+modulus = integer % float
+exponentiation = integer ** float
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+console.log(modulus)
+console.log(exponentiation)
