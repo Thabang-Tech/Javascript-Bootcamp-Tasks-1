fe1e824b043b7070ffd1cef8b8e3735509b9040f
Thabang Matlou <thabangmatlou29@gmail.com>
2026-05-27 08:16:40 +0200

    Update Variables

--- variables.js
+++ variables.js
@@ -1,3 +1,5 @@
+//Variable Declaration
+
 let name = 'Thabang'
 let age = 33
 let isStudent = true
@@ -5,3 +7,18 @@ let isStudent = true
 console.log(name)
 console.log(age)
 console.log(isStudent)
+
+// Variable Reassignment
+
+let favoriteColor = 'White'
+
+console.log(favoriteColor)
+
+favoriteColor = 'Blue'
+
+console.log(favoriteColor)
+
+// Arithmatic Operations
+
+num1 = 10
+num2 = 20
