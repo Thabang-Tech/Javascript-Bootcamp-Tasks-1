23a0889c1623e84b880f3cf1e3e8f1b26c489d08
Thabang Matlou <thabangmatlou29@gmail.com>
2026-05-29 19:11:42 +0200

    Conditinal update

--- conditionalStatements_loops.js
+++ conditionalStatements_loops.js
@@ -1,16 +1,42 @@
-//Temperature check
+//1. Temperature check
+    //(i). If...
 
 let temperature = 0
+let weather
 
 if (temperature < 0){
-    console.log('It is freezing')
+    weather = "freezing"
 }
 else if (temperature >= 0 && temperature <= 15){
-    console.log('It is cold')
+    weather = "cold"
 }
 else if (temperature >= 16 && temperature <= 25){
-    console.log('It is mild')
+    weather = "mild"
 }
 else if (temperature > 25){
-    console.log('It is warm')
+    weather = "warm"
+}
+
+  //()
+
+switch (weather){
+
+    case "freezing":
+        console.log("It is freezing")
+        break
+
+    case "cold":
+        console.log("It is cold")
+        break
+
+    case "mild":
+        console.log("It is mild")
+        break
+
+    case "warm":
+        console.log("It is warm")
+        break
+
+    default:
+        console.log("Unknown weather")
 }
