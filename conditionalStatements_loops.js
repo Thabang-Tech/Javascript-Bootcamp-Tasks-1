58d673d6e7981b94273f1926570431783489a01b
Thabang Matlou <thabangmatlou29@gmail.com>
2026-05-29 20:19:45 +0200

    Conditinal update

--- conditionalStatements_loops.js
+++ conditionalStatements_loops.js
@@ -45,3 +45,42 @@ switch (true){
     default:
         console.log("Unknown weather")
 }
+
+//2 (i)
+
+let number = 12
+
+if (number % 2 === 0 && number % 3 === 0){
+    console.log("Divisible by both")
+}
+else if (number % 2 === 0){
+    console.log("Divisible by 2")
+}
+else if (number % 3 === 0){
+    console.log("Divisible by 3")
+}
+else{
+    console.log("Not divisible by 2 or 3")
+}
+
+// (ii)
+
+let number = 12
+
+switch(true){
+
+    case (number % 2 === 0 && number % 3 === 0):
+        console.log("Divisible by both")
+        break
+
+    case (number % 2 === 0):
+        console.log("Divisible by 2")
+        break
+    
+    case (number % 3 === 0):
+        console.log("Divisible by 3")
+        break
+    
+    default:
+        console.log("Not divisible by 2 or 3")
+}
